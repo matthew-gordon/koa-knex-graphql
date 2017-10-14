34d59db8df551b0e4c78d2fc266feaa54e3442cb
Matthew Gordon <laxredemption5525@gmail.com>
2017-10-14 09:44:19 -0600

    Setup grapiql client

--- src/server/index.js
+++ src/server/index.js
@@ -2,15 +2,16 @@ const Koa = require('koa')
 const Router = require('koa-router')
 const bodyParser = require('koa-bodyparser')
 
+const { graphqlKoa, graphiqlKoa } = require('apollo-server-koa')
+
 const app = new Koa()
 const port = process.env.PORT || 3000
 const router = new Router()
 
-router.get('/', async (ctx) => {
-   ctx.body = {
-     message: 'Hello world!'
-   }
-})
+app.use(bodyParser())
+
+router.get('/graphiql', graphiqlKoa({ endpointURL: '/graphql'}))
+// router.post('/graphql', graphqlKoa({ schema: GraphQLSchema}))
 
 app.use(router.routes())
 app.use(router.allowedMethods())
@@ -18,3 +19,5 @@ app.use(router.allowedMethods())
 const server = app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
 })
+
+module.exports = server
